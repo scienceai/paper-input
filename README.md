448177f39fd8e123c050e75cdd24583db6de31f4
Chris Callahan <callahan.c@gmail.com>
2015-09-18 18:22:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# <PaperInput />
+
+### Install
+```
+npm install paper-input
+```
+
+### Using the Component
+```js
+import PaperInput from 'paper-input';
+```
+
+### Using the CSS
+```css
+@import "../node_modules/paper-input/dist/paper-input.css";
+```
+
+### API
+* `label: String`: Required. The label that will be displayed on the `<input>` element.
+* `name: String`: Required. The `name` attribute that will be attached to the `<input>` element.
+* `className: String`: Optional.
+* `defaultValue: String`: Optional.
+* `error: String`: Optional. An error message that is displayed below the component.
+* `floatLabel: Boolean`: Optional. Floats the label to the top of the element when focused.
+* `onBlurCapture: Function`: Optional. Called on the `blur` event on the `<input>`.
+* `onKeyPress: Function`: Optional. Called on the `keypress` event on the `<input>`.
+* `onChange: Function`: Optional. Called on the `change` event on the `<input>`.
+* `type: String`: Optional. Defaults to `'text'`.
+* `value: String`: Optional.
+
+### Example
+```js
+<PaperInput
+  name='email'
+  label='Email Address',
+  type='email'
+  floatLabel={true}
+  error={this.state.error ? 'Please enter a valid email address' : ''}
+  onChange={this.handleChange.bind(this)}
+  value={this.state.email}
+/>
+```
---
+++ src/paper-input.js
@@ -0,0 +1,109 @@
+import React, { Component, findDOMNode, PropTypes } from 'react';
+
+export default class PaperInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      touched: false,
+      dirty: ('defaultValue' in props) ? !!props.defaultValue : !!props.value
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      dirty: (('defaultValue' in nextProps) ? !!nextProps.defaultValue : !!nextProps.value) || !!findDOMNode(this.refs.input).value,
+      touched: (nextProps.value)? this.state.touched : false
+    });
+  }
+
+  componentDidUpdate() {
+    let input = findDOMNode(this.refs.input);
+    if (this.props.error) {
+      input.setCustomValidity(this.props.error);
+    } else {
+      input.setCustomValidity('');
+    }
+  }
+
+  handleBlurCapture(e) {
+    this.setState({ dirty: !!e.target.value }, () => {
+      if (this.props.onBlurCapture) {
+        this.props.onBlurCapture(e);
+      }
+    });
+  }
+
+  handleKeyPress(e) {
+    if (this.props.onKeyPress) {
+      this.props.onKeyPress(e);
+    }
+
+    if (!this.state.touched) {
+      this.setState({ touched: true });
+    }
+  }
+
+  handleChange(e) {
+    if (this.props.onChange) {
+      this.props.onChange(e);
+    }
+
+    if (e.target.value && !this.state.dirty) {
+      this.setState({ dirty: true });
+    }
+
+    if (!e.target.value && this.state.dirty) {
+      this.setState({ dirty: false });
+    }
+  }
+
+  //convenience method to be called by a container component
+  cancel() {
+    findDOMNode(this.refs.input).value = '';
+    this.setState({ dirty: false });
+  }
+
+  getValue() {
+    return findDOMNode(this.refs.input).value;
+  }
+
+  render() {
+    let className = this.props.className || '';
+
+    return (
+      <div className={`paper-input${this.props.floatLabel ? ' float-label' : ''}${className ? ` ${className}` : ''}`}>
+        <input
+          {...this.props}
+          ref='input'
+          className={`${className}${this.state.dirty ? ' dirty' : ''}${this.state.touched ? ' touched' : ''}`}
+          onBlurCapture={this.handleBlurCapture.bind(this)}
+          onKeyPress={this.handleKeyPress.bind(this)}
+          onChange={this.handleChange.bind(this)}
+        />
+        <label htmlFor={this.props.name}>{this.props.label}</label>
+        <span className='error'>{this.props.error}</span>
+      </div>
+    );
+  }
+}
+
+PaperInput.propTypes = {
+  className: PropTypes.string,
+  defaultValue: PropTypes.string,
+  error: PropTypes.string,
+  floatLabel: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onBlurCapture: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  onChange: PropTypes.func,
+  type: PropTypes.string,
+  value: PropTypes.string
+};
+
+PaperInput.defaultProps = {
+  error: '',
+  floatLabel: true,
+  type: 'text'
+};
