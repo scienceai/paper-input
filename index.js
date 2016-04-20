16c2c1bc23ae298d95fcdc60edc9aa3fa5a04fa9
Chris Callahan <callahan.c@gmail.com>
2016-04-20 11:40:48 -0400

    have large text be opt-in, update readme and example

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PaperInput  from '../src';
 
+function Row({ children }) {
+  return (
+    <div style={{height: '60px'}}>
+      {children}
+    </div>
+  );
+}
+
 class App extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -18,14 +26,48 @@ class App extends React.Component {
   render() {
     return (
       <section style={{margin: '50px auto', width: '50vw'}}>
-        <PaperInput
-          name='email'
-          label='Email Address'
-          type='email'
-          placeholder='me@example.com'
-          floatLabel={true}
-          onChange={this.handleChange.bind(this)}
-        />
+        <Row>
+          <PaperInput
+            name='normal-text'
+            label='Normal Text'
+            onChange={this.handleChange}
+          />
+        </Row>
+        <Row>
+          <PaperInput
+            name='large-text'
+            label='Large Text'
+            large={true}
+            onChange={this.handleChange}
+          />
+        </Row>
+        <Row>
+          <PaperInput
+            name='no-float'
+            label='No Float Label'
+            large={true}
+            floatLabel={false}
+            onChange={this.handleChange}
+          />
+        </Row>
+        <Row>
+          <PaperInput
+            name='with-placeholder'
+            label='With Placeholder'
+            large={true}
+            placeholder='Hello World'
+            onChange={this.handleChange}
+          />
+        </Row>
+        <Row>
+          <PaperInput
+            name='email'
+            label='Email Address'
+            type='email'
+            large={true}
+            onChange={this.handleChange}
+          />
+        </Row>
       </section>
     )
   }
